ae4936476e033cca53d9e721c1016f12cc5c91fa
kalyan021004 <kalyan021004@gmail.com>
2026-02-26 21:45:37 +0530

    final deploy

--- routes/game.js
+++ routes/game.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const Score = require("../models/Score");
 
 router.get("/", (req, res) => {
-  if (!req.session.user) return res.redirect("/");
   res.render("dashboard");
 });
 
@@ -10,14 +9,6 @@ router.get("/play/:game", (req, res) => {
   res.render("game", { game: req.params.game });
 });
 
-router.post("/score", async (req, res) => {
-  await Score.create({
-    user: req.session.user.name,
-    game: req.body.game,
-    score: req.body.score
-  });
 
-  res.redirect("/leaderboard");
-});
 
 module.exports = router;
